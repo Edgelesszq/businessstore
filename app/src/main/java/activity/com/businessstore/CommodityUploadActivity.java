5e27d5231fb69a14466646e9dcf81de5ac005b4b
Wx <546030736@qq.com>
2018-07-10 09:52:23 +0800

    7.10

--- app/src/main/java/activity/com/businessstore/CommodityUploadActivity.java
+++ app/src/main/java/activity/com/businessstore/CommodityUploadActivity.java
@@ -1,10 +1,16 @@
 package activity.com.businessstore;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class CommodityUploadActivity extends BaseActivity implements View.OnClickListener {
+    private EditText editTitle;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +21,32 @@ public class CommodityUploadActivity extends BaseActivity implements View.OnClic
     private void initView() {
         setTitleView(R.drawable.backimage,R.string.commodity_upload,R.string.save);
         mTitleLefeBackImg.setOnClickListener(this);
+        editTitle = findViewById(R.id.edit_commodity_title);
+//        final String editContext = editTitle.getText().toString();
+        final Editable editContext = editTitle.getText();
+        editTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s == editContext){
+                    mTitleRightText.setTextColor(Color.parseColor("#FDBA43"));
+                    Log.d("lllll","同");
+                }else {
+                    mTitleRightText.setTextColor(Color.parseColor("#000000"));
+                    Log.d("lllll","不同");
+                }
+
+            }
+        });
     }
 
     @Override
@@ -26,4 +58,6 @@ public class CommodityUploadActivity extends BaseActivity implements View.OnClic
         }
 
     }
+
+
 }
