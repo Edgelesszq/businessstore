0bb1ebeaf4f90f55cd975bcff5c126bd3c244cdf
jacky <zq2018work@163.com>
2018-07-19 11:15:41 +0800

    修改ui,交互

--- app/src/main/java/activity/com/businessstore/AccountUpadatePhoneNumActivity.java
+++ app/src/main/java/activity/com/businessstore/AccountUpadatePhoneNumActivity.java
@@ -1,12 +1,21 @@
 package activity.com.businessstore;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class AccountUpadatePhoneNumActivity extends BaseActivity implements View.OnClickListener{
     private Context mContext;
+    private ImageView clean_iv;
+    private EditText phonenum_et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +26,53 @@ public class AccountUpadatePhoneNumActivity extends BaseActivity implements View
         mContext = this;
         setTitleView(R.drawable.backimage,R.string.update_phonenum,R.string.save);
         mTitleLefeBackImg.setOnClickListener(this);
+        mTitleRightText.setOnClickListener(this);
+
+        clean_iv=findViewById(R.id.clean_iv);
+        clean_iv.setOnClickListener(this);
+        phonenum_et=findViewById(R.id.phonenum_et);
+        phonenum_et.setOnClickListener(this);
+
+        final String phonenumstr=getIntent().getStringExtra("phoneNum").trim();
+        phonenum_et.setText(phonenumstr);
+        phonenum_et.setSelection(phonenum_et.getText().length());
+        TextWatcher watcher=new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length()>0&&s.toString()!=phonenumstr&&!s.toString().equals(phonenumstr)){
+                    clean_iv.setVisibility(View.VISIBLE);
+                    mTitleRightText.setClickable(true);
+                    mTitleRightText.setTextColor(getBaseContext().getResources().getColor(R.color.nav_color));
+
+                }
+                else if(s.length()>0&&s.toString()==phonenumstr&&s.toString().equals(phonenumstr))
+                {
+                    clean_iv.setVisibility(View.VISIBLE);
+                    mTitleRightText.setClickable(false);
+                }
+
+                else{
+                    clean_iv.setVisibility(View.GONE);
+                    mTitleRightText.setClickable(false);
+                        //mTitleRightText.setTextColor(R.color.nav_color);
+                    mTitleRightText.setTextColor(getBaseContext().getResources().getColor(R.color.nav_fontcolor));
+
+
+
+                }
+            }
+        };
+        phonenum_et.addTextChangedListener(watcher);
     }
 
 
@@ -24,7 +80,14 @@ public class AccountUpadatePhoneNumActivity extends BaseActivity implements View
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.title_left_back_img:
+
                 this.finish();
+                break;
+            case R.id.clean_iv:
+                phonenum_et.setText("");
+                break;
+            case R.id.title_right_text:
+                Toast.makeText(this,"sssss",Toast.LENGTH_SHORT).show();
                 break;
         }
     }
