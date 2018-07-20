752b5b8bd4f16ba1ec4f0de8b89bee3c95d98bce
laze1 <546030736@qq.com>
2018-07-20 10:29:46 +0800

    shareSDK

--- app/src/main/java/activity/com/businessstore/MainSearchActivity.java
+++ app/src/main/java/activity/com/businessstore/MainSearchActivity.java
@@ -47,6 +47,7 @@ import java.util.TreeSet;
 import adapter.com.businessstore.AdapterMainActivity;
 import adapter.com.businessstore.AdapterSearchActivity;
 import adapter.com.businessstore.AdapterSearchResultActivity;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
 public class MainSearchActivity extends BaseActivity implements View.OnClickListener {
@@ -222,7 +223,25 @@ public class MainSearchActivity extends BaseActivity implements View.OnClickList
                 mPopwindowIsShow = true;
                 break;
             case R.id.main_recyclerview_item_more_pop_share:
-                Toast.makeText(mContext, "分享", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mContext, "分享", Toast.LENGTH_SHORT).show();
+                OnekeyShare oks = new OnekeyShare();
+                //关闭sso授权
+                oks.disableSSOWhenAuthorize();
+
+                // title标题，微信、QQ和QQ空间等平台使用
+                oks.setTitle("分享测试标题");
+                // titleUrl QQ和QQ空间跳转链接
+                oks.setTitleUrl("http://sharesdk.cn");
+                // text是分享文本，所有平台都需要这个字段
+                oks.setText("我是分享文本");
+                // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+                oks.setImagePath("http://img5.imgtn.bdimg.com/it/u=471547240,4067919551&fm=27&gp=0.jpg");//确保SDcard下面存在此张图片
+                // url在微信、微博，Facebook等平台中使用
+                oks.setUrl("http://sharesdk.cn");
+                // comment是我对这条分享的评论，仅在人人网使用
+//                oks.setComment("我是测试评论文本");
+                // 启动分享GUI
+                oks.show(this);
                 popWindow.dismiss();
                 mPopwindowIsShow = true;
                 break;
