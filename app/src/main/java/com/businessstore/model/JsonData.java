a377dd0dacffb8a829a077d76b8b56f13133aaa3
laze1 <546030736@qq.com>
2018-07-31 13:23:37 +0800

    login cookies

---
+++ app/src/main/java/com/businessstore/model/JsonData.java
@@ -0,0 +1,15 @@
+package com.businessstore.model;
+
+import java.util.List;
+
+public class JsonData {
+    private DataInfo data;
+
+    public DataInfo getData() {
+        return data;
+    }
+
+    public void setData(DataInfo data) {
+        this.data = data;
+    }
+}
